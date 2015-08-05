3d9a54feb45d9dc2d5c626a90e0633bbbf85c7ae
Salvatore Candela <salvatore@soekan.com>
2015-08-05 00:44:52 -0700

    updated script calls

--- function-snippets.php
+++ function-snippets.php
@@ -8,8 +8,8 @@
         // This theme uses wp_nav_menu() in two locations.
         // This snippet supports the two navs in proper-nav.php
         register_nav_menus( array(
-            'primary' => __( 'Primary Menu', 'zupgroup' ),
-            'mobile' => __( 'Mobile Menu', 'zupgroup' )
+            'primary' => __( 'Primary Menu', 'my_theme_name' ),
+            'mobile' => __( 'Mobile Menu', 'my_theme_name' )
         ) );
 
     }
@@ -18,9 +18,9 @@
 
 
     function menu_scripts() {
-        wp_enqueue_script( 'mmain-js', get_template_directory_uri() . '/assets/main.js', array(), '20150701', true);
+        wp_enqueue_script( 'main-js', get_template_directory_uri() . '/assets/main.js', array(), '20150701', true);
         
-        wp_enqueue_style( 'mmenu-css', get_template_directory_uri() . '/assetsjquery.mmenu.css', array(), '20150701');
+        wp_enqueue_style( 'mmenu-css', get_template_directory_uri() . '/assets/jquery.mmenu.css', array(), '20150701');
         wp_enqueue_script( 'mmenu-js', get_template_directory_uri() . '/assets/jquery.mmenu.min.js', array(), '20150701', true);
         
     }
